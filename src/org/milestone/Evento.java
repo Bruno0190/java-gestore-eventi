b027b61a3a2140265fe39cc81f12aa6e9ea50504
Bruno0190 <bruno.varadero90@gmail.com>
2025-05-31 19:51:41 +0200

    feat: prima parte

---
+++ src/org/milestone/Evento.java
@@ -0,0 +1,83 @@
+package org.milestone;
+
+import java.time.LocalDate;
+
+public class Evento {
+
+    //Affinchè gli attributi della classe non siano accessibili direttamente è buona regola scriverli come private, in modo da potervi poi accedere tramite getter e setter, quest'ultimo consente di filtrare le modifiche del valore con opportuni controlli. Certamente, se sapessimo che la classe deve essere ereditata si può usare protected, ma spesso ciò è più utile qualora la classe fosse astratta.
+    private String titolo;
+    //Per l'attributo data mi sono avvalso della classe LocalDate del package java.time (che infatti è stato importato in alto.)
+    private LocalDate data;
+    private int postiTotali;
+    private int postiPrenotati;
+
+    //Nel costruttore passiamo come parametri gli attributi necessari per istanziare un oggetto. La traccia dice di inizializzare postiPrenotati a 0, ergo non è necessario includere quest'attributo tra i parametri.
+    public Evento(String titolo, LocalDate data, int postiTotali){
+
+        //Ricordiamo che "this" occorre perchè titolo, data, postiTotali e postiPrenotati si riferiscano proprio ai rispettivi attributi sopra elencati della classe. A destra dell'uguale infatti titolo, data e postiTotali si riferiscono ai parametri del costruttore. "this" non viene usato solo se il parametro del costruttore non corrisponde all'attributo della classe.
+        this.titolo = titolo;
+        this.data = data;
+        this.postiTotali = postiTotali;
+        this.postiPrenotati = 0;
+
+        /*Di seguito abbiamo dei controlli che come secondo traccia verificano che la data immessa come parametro non sia antecedente a quella odierna e che i posti totali del posto ove avverrà luogo l'evento non siano negativi, ma nemmeno nulli.
+        I controlli funzionano grazie a "IllegalArgumentException" che di fatto è una sottocalsse della classe Exception. Infatti per essere istanziata si "lancia" con throw e quindi creata con new*/       
+        if (data.isBefore(LocalDate.now())){
+
+            throw new IllegalArgumentException("Non puoi inserire una data passata");
+        
+        }
+
+        if (postiTotali<1){
+
+            throw new IllegalArgumentException("Devi inserire numeri positivi");
+
+        }
+
+    }
+
+    //Si passa ai metodi getter e setter come da traccia. Questi metodi sono public ovviamente perchè sono i metodi tramite i quali nel Main possiamo accedere e modificare gli attributi. getTitolo consente di leggere il titolo, infatti "ritorna" la stringa titolo. setTitolo invece consente di reimpostare il titolo che è una stringa, non tornando nulla tra le graffe, è un metodo void. Stesso discorso per gli altri metodi
+    public String getTitolo(){
+
+        return titolo;
+
+    }
+
+    public void setTitolo(String titolo){
+
+        this.titolo = titolo;
+
+    }
+
+    public LocalDate getData(){
+
+        return data;
+
+    }
+    //Il controllo della data corretta deve assolutamente essere inserito anche nel setter
+    public void setData(LocalDate data){
+
+        if (data.isEqual(LocalDate.now()) || data.isAfter(LocalDate.now())){
+
+            this.data = data;
+
+        } else {
+
+            throw new IllegalArgumentException("Non puoi inserire una data passata");
+
+        }
+
+    }
+
+    public int getPostiTot(){
+
+        return postiTotali;
+
+    }
+
+    public int getPostiBooked(){
+
+        return postiPrenotati;
+
+    }
+}
